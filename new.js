5d8ad88413d4c493d917caee68f698ad8a5d09d2
Deepakab17 <deepaknab17@gmail.com>
2025-12-19 23:27:31 +0530

    new

---
+++ new.js
@@ -0,0 +1,59 @@
+// Q . 26
+
+// let num =5;
+// let fact= 1;
+
+// for (let i = 1; i <= num; i++) {
+//     fact = fact*i;
+// }
+
+// console.log(fact);
+
+
+// / Q . 27
+
+
+// let num2 = 12345;
+// let count = 0;
+
+// while (num2 > 0) {
+//     count++;
+//     num2 = Math.floor(num2 / 10);
+// }
+
+// console.log(count);
+
+
+// / Q . 28
+
+// let year = 1998;
+
+// if ((year % 4===0 && year % 100!==0) || year % 400===0) {
+//     console.log("Leap Year");
+// } else {
+//     console.log("Not a Leap Year");
+// }
+
+
+// / Q . 29
+
+// let ray = [];
+
+// for (let i = 1; i <= 10; i++) {
+//     ray.push(i);
+// }
+
+// for (let num of ray) {
+//     if (num % 2 !== 0) {
+//         console.log(num);
+//     }
+// }
+
+
+// / Q . 30
+
+
+// let sen = "this is javascript";
+// let words = sen.split(" ");
+
+// console.log(words.length);
